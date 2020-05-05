334541cc844d2015a704bd37efe57dba300527ab
sugeta10 <sugeta@coincheck.com>
2020-05-05 15:27:27 +0900

    v-on,v-model,v-ifの使い方をメモ

--- main.js
+++ main.js
@@ -2,6 +2,20 @@ var app = new Vue ({
   el:'#app',
   data:{
     message: 'Hello, Vue.js!',
-    list: ['Ruby','Python','JavaScript']
+    list: ['Ruby','Python','JavaScript'],
+    input_text: 'Here You written',
+    show: true
+  },
+  methods:{
+    handleClick: function(event){
+      alert(event.target)
+    },
+    switchShow: function(event){
+      if (app.show == true) {
+        app.show = false;
+        } else {
+        app.show = true;
+        }
+    }
   }
 })
