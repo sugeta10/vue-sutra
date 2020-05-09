1c3a03b8cc6a6e7422d26317655606ce33422c4d
sugeta10 <sugeta@coincheck.com>
2020-05-09 10:24:41 +0900

    2章途中まで

--- main.js
+++ main.js
@@ -1,21 +1,75 @@
 var app = new Vue ({
+  // Applicationを紐付ける要素のセレクタ/mountする要素
   el:'#app',
+  // アプリケーションで使用するデータ
   data:{
     message: 'Hello, Vue.js!',
     list: ['Ruby','Python','JavaScript'],
     input_text: 'Here You written',
-    show: true
+    show: true,
+    show_t: true
   },
+  // アプリケーションで使用するメソッド
   methods:{
     handleClick: function(event){
       alert(event.target)
     },
-    switchShow: function(event){
+    switchShow: function(){
       if (app.show == true) {
         app.show = false;
         } else {
         app.show = true;
         }
     }
+  },
+  // 算出プロパティ dataと似たように扱うことのできるデータ
+  computed: {
+    computedMessage: function(){
+      // なにか処理をした結果をデータとして返す
+      return this.message + '!'
+    }
+  },
+  // ライフサイクルフック 色々あるがcreated では初期化が終わったら呼び出される
+  created: function(){
+    // 行いたい処理
   }
+})
+
+
+var app2 = new Vue ({
+    el:'#app2',
+    data:{
+      message: 'Hello, Vue.js!',
+      scroll: 0,
+      count: 0,
+      isChild: true,
+      isActive: true,
+      textColor: 'red',
+      bgColor: 'lightgray',
+      radius: 50,
+      item: {
+        id: 1,
+        src: 'item.jpeg',
+        alt: '商品１',
+        width: 100,
+        height: 100
+      },
+      classObject: {
+        child: true,
+        'is-active': true,
+      },
+      styleObject: {
+        color: 'red',
+        backgroundColor: 'lightgray',
+      }
+    },
+    methods: {
+      increment: function(){
+        this.count += 1
+      }
+    },
+    // 要素のスクロール量の操作のところはよくわかってない・・・
+    mounted: function(){
+      this.scroll = 100 //要素のスクロール量
+    },
 })
